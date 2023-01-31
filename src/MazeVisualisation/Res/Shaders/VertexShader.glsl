91ee5d6fd6786f31147753fdb49916f74ba22e8a
Ryan Thomas Payne <50511769+Ry0511@users.noreply.github.com>
2023-01-31 18:36:40 +0000

    Moving the functionality of the Main class to utilise 3D Projection of a pre-defined cube.

--- src/MazeVisualisation/Res/Shaders/VertexShader.glsl
+++ src/MazeVisualisation/Res/Shaders/VertexShader.glsl
@@ -1,16 +1,24 @@
 #version 330 core
 
-layout (location = 0) in vec2 v_pos;
+// From Buffers
+layout (location = 0) in vec3 v_pos;
 layout (location = 1) in vec3 v_colour;
-
-uniform float u_Theta;
 
 out vec3 colour;
 
+// Matrices
+uniform mat4 u_ProjectionMatrix;
+uniform mat4 u_ViewMatrix;
+uniform mat4 u_ModelMatrix;
+
+// Excess
+uniform float u_Theta;
+
 void main() {
-    float sin_offset = abs(sin(u_Theta * 2));
+    colour = v_colour;
 
-    colour = v_colour * sin_offset;
+    // vec4 temp = vec4(v_pos, 1.0, 1.0) * u_RotationMatrix;
+    // gl_Position = vec4(temp.x, temp.y, 1.0, 1.0);
 
-    gl_Position = vec4(v_pos, 1.0, 1.0);
+    gl_Position = u_ProjectionMatrix * u_ViewMatrix * u_ModelMatrix * vec4(v_pos, 1.0);
 }
