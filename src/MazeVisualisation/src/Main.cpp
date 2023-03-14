5a22c28dbdf68265cc94061321e8ab9908630c84
Ryan Thomas Payne <50511769+Ry0511@users.noreply.github.com>
2023-03-14 13:27:04 +0000

    Camera Position is updated in the window title.

--- src/MazeVisualisation/src/Main.cpp
+++ src/MazeVisualisation/src/Main.cpp
@@ -36,11 +36,15 @@ public:
 
     virtual bool on_update(float delta) override {
 
+        const auto& cam = get_camera_state();
         set_title(
                 std::format(
-                        "Window # {:<4} fps, Delta: {:<2.2f} ms",
+                        "Window # {:<4} fps, Delta: {:<2.2f} ms, Pos: {:.2f}, {:.2f}, {:.2f}",
                         (int) (1.0 / (delta)),
-                        delta * 1000.F
+                        delta * 1000.F,
+                        cam.cam_pos.x,
+                        cam.cam_pos.y,
+                        cam.cam_pos.z
                 ).c_str()
         );
 
