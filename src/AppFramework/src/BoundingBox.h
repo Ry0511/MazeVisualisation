d2122797b5bc621401ea33e44b8f88122609197d
Ryan Thomas Payne <50511769+Ry0511@users.noreply.github.com>
2023-03-14 13:25:06 +0000

    Simple AxisAlignedBoundingBox class which can perform intersection tests.

---
+++ src/AppFramework/src/BoundingBox.h
@@ -0,0 +1,75 @@
+//
+// Created by -Ry on 14/03/2023.
+//
+
+#ifndef MAZEVISUALISATION_BOUNDINGBOX_H
+#define MAZEVISUALISATION_BOUNDINGBOX_H
+
+#include <string>
+#include <format>
+
+#include <glm/glm.hpp>
+
+namespace app {
+
+    class AxisAlignedBoundingBox {
+
+    private:
+        glm::vec3 m_Min;
+        glm::vec3 m_Max;
+
+    public:
+
+        AxisAlignedBoundingBox(
+                const glm::vec3& min,
+                const glm::vec3& max
+        ) : m_Min(min), m_Max(max) {
+
+        }
+
+        AxisAlignedBoundingBox(
+                const glm::vec3& centre,
+                const float offset
+        ) : m_Min(centre.x - offset, centre.y - offset, centre.z - offset),
+            m_Max(centre.x + offset, centre.y + offset, centre.z + offset) {
+
+        }
+
+    public:
+
+        void realign(const glm::vec3& centre, const float offset) {
+            m_Min = glm::vec3(centre.x - offset, centre.y - offset, centre.z - offset);
+            m_Max = glm::vec3(centre.x + offset, centre.y + offset, centre.z + offset);
+        }
+
+        void realign(const glm::vec3& min, const glm::vec3& max) {
+            m_Min = min;
+            m_Max = max;
+        }
+
+    public:
+
+        bool intersects(const AxisAlignedBoundingBox& o) const {
+            return m_Min.x <= o.m_Max.x
+                   && m_Max.x >= o.m_Min.x
+                   && m_Min.y <= o.m_Max.y
+                   && m_Max.y >= o.m_Min.y
+                   && m_Min.z <= o.m_Max.z
+                   && m_Max.z >= o.m_Min.z;
+        }
+
+
+    public:
+
+        std::string to_string() const {
+            return std::format(
+                    "Min=( {:.2f}, {:.2f}, {:.2f} ),"
+                    " Max=( {:.2f}, {:.2f}, {:.2f} )",
+                    m_Min.x, m_Min.y, m_Min.z,
+                    m_Max.x, m_Max.y, m_Max.z
+            );
+        }
+    };
+}
+
+#endif //MAZEVISUALISATION_BOUNDINGBOX_H
