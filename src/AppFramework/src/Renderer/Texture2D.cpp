8b8fe871702a8f8fe6168852fe2b90e01930966d
Ryan Thomas Payne <50511769+Ry0511@users.noreply.github.com>
2023-03-18 10:47:00 +0000

    Added a Texture Class which wraps an OpenGL texture.

---
+++ src/AppFramework/src/Renderer/Texture2D.cpp
@@ -0,0 +1,92 @@
+//
+// Header File: Texture2D.cpp
+// Date       : 18/03/2023
+// Project    : MazeVisualisation
+// Author     : -Ry
+//
+
+#include "Texture2D.h"
+
+#include "Logging.h"
+#include "GLUtil.h"
+#include "Image.h"
+
+#include <gl/glew.h>
+
+namespace app {
+
+    //############################################################################//
+    // | CONSTRUCTORS |
+    //############################################################################//
+
+    Texture2D::Texture2D() : m_Texture() {
+        GL(glGenTextures(1, &m_Texture));
+        ASSERT(m_Texture != 0U, "Generated Texture is invalid...");
+        HINFO("[TEXTURE2D]", " # Created Texture '{}'...", m_Texture);
+    }
+
+    Texture2D::Texture2D(Texture2D&& other) : m_Texture(other.m_Texture) {
+        other.m_Texture = 0U;
+    }
+
+    //############################################################################//
+    // | ASSIGNMENT OPERATOR |
+    //############################################################################//
+
+    Texture2D& Texture2D::operator =(Texture2D&& other) {
+        if (m_Texture != 0U) {
+            GL(glDeleteTextures(1, &m_Texture));
+        }
+        m_Texture = other.m_Texture;
+        other.m_Texture = 0U;
+        return *this;
+    }
+
+    //############################################################################//
+    // | UTILITY FUNCTIONS |
+    //############################################################################//
+
+    void Texture2D::bind(TextureUnit unit) {
+        GL(glActiveTexture((GLenum) unit));
+        GL(glBindTexture(GL_TEXTURE_2D, m_Texture));
+        s_BoundTexture[(GLenum) unit] = m_Texture;
+    }
+
+    void Texture2D::unbind(TextureUnit unit) {
+        GL(glActiveTexture((GLenum) unit));
+        GL(glBindTexture(GL_TEXTURE_2D, 0));
+        s_BoundTexture[(GLenum) unit] = 0;
+    }
+
+    bool Texture2D::is_bound(TextureUnit unit) {
+        return s_BoundTexture[(GLenum) unit] == m_Texture;
+    }
+
+    void Texture2D::set_texture_image(const Image& image) {
+        ASSERT(is_bound(), "Texture must be bound before setting data...");
+        GL(
+                glTexImage2D(
+                        GL_TEXTURE_2D,
+                        0,
+                        GL_RGB,
+                        image.get_width(),
+                        image.get_height(),
+                        0,
+                        GL_RGB,
+                        GL_UNSIGNED_BYTE,
+                        (void*) image.get_pixel_data()
+                )
+        );
+        GL(glGenerateMipmap(GL_TEXTURE_2D));
+    }
+
+    void Texture2D::set_texture_flags(
+            std::initializer_list<std::pair<TextureFlag, GLint>> flags
+    ) {
+        ASSERT(is_bound(), "Texture must be bound when setting flags...");
+        for (const auto [flag, value] : flags) {
+            GL(glTexParameteri(GL_TEXTURE_2D, (GLenum) flag, value));
+        }
+    }
+
+} // app
---
+++ src/AppFramework/src/Renderer/Texture2D.h
@@ -0,0 +1,88 @@
+//
+// Header File: Texture2D.h
+// Date       : 18/03/2023
+// Project    : MazeVisualisation
+// Author     : -Ry
+//
+
+#ifndef MAZEVISUALISATION_TEXTURE2D_H
+#define MAZEVISUALISATION_TEXTURE2D_H
+
+#include <gl/glew.h>
+#include <initializer_list>
+#include <utility>
+#include <array>
+
+namespace app {
+
+    class Image;
+
+    //############################################################################//
+    // | GLEW TEXTURE 2D ENUMERATIONS |
+    //############################################################################//
+
+    // See: https://registry.khronos.org/OpenGL-Refpages/gl4/html/glTexParameter.xhtml
+    // for potential values
+    enum class TextureFlag : GLenum {
+        BASE_LEVEL   = GL_TEXTURE_BASE_LEVEL,
+        COMPARE_FUNC = GL_TEXTURE_COMPARE_FUNC,
+        COMPARE_MODE = GL_TEXTURE_COMPARE_MODE,
+        LOD_BIAS     = GL_TEXTURE_LOD_BIAS,
+        MIN_LOD      = GL_TEXTURE_MIN_LOD,
+        MAX_LOD      = GL_TEXTURE_MAX_LOD,
+        WRAP_S       = GL_TEXTURE_WRAP_S,
+        WRAP_T       = GL_TEXTURE_WRAP_T,
+        MIN_FILTER   = GL_TEXTURE_MIN_FILTER,
+        MAG_FILTER   = GL_TEXTURE_MAG_FILTER
+    };
+
+    enum class TextureUnit : GLenum {
+        ZERO  = GL_TEXTURE0,
+        ONE   = GL_TEXTURE1,
+        TWO   = GL_TEXTURE2,
+        THREE = GL_TEXTURE3,
+        FOUR  = GL_TEXTURE4,
+        FIVE  = GL_TEXTURE5,
+        SIX   = GL_TEXTURE6,
+        SEVEN = GL_TEXTURE7,
+        EIGHT = GL_TEXTURE8,
+        NINE  = GL_TEXTURE9,
+        TEN   = GL_TEXTURE10
+    };
+
+    inline static constexpr size_t s_TextureUnitCount = 10;
+
+    //############################################################################//
+    // | TEXTURE WRAPPER CLASS |
+    //############################################################################//
+
+    class Texture2D {
+
+    private:
+        inline static std::array<unsigned int, s_TextureUnitCount> s_BoundTexture{ 0U };
+
+    private:
+        unsigned int m_Texture;
+
+    public:
+        Texture2D();
+        Texture2D(const Texture2D&) = delete;
+        Texture2D(Texture2D&&);
+
+    public:
+        Texture2D& operator =(const Texture2D&) = delete;
+        Texture2D& operator =(Texture2D&&);
+
+    public:
+        void bind(TextureUnit unit = TextureUnit::ZERO);
+        void unbind(TextureUnit unit = TextureUnit::ZERO);
+        bool is_bound(TextureUnit unit = TextureUnit::ZERO);
+
+    public:
+        void set_texture_image(const Image& image);
+        void set_texture_flags(std::initializer_list<std::pair<TextureFlag, GLint>> flags);
+    };
+
+} // app
+
+#endif
