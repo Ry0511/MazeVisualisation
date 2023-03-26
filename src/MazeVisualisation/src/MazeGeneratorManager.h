69848f669bf10525c0b8350114c1db8a852a47b5
Ryan Thomas Payne <50511769+Ry0511@users.noreply.github.com>
2023-03-26 13:26:07 +0100

    MazeGeneratorManager class to replace the inner class of MazeManager.

---
+++ src/MazeVisualisation/src/MazeGeneratorManager.h
@@ -0,0 +1,39 @@
+//
+// Header File: MazeGeneratorManager.h
+// Date       : 26/03/2023
+// Project    : MazeVisualisation
+// Author     : -Ry
+//
+
+#ifndef MAZEVISUALISATION_MAZEGENERATORMANAGER_H
+#define MAZEVISUALISATION_MAZEGENERATORMANAGER_H
+
+#include "Application.h"
+#include "MazeConstructs.h"
+
+namespace maze {
+
+    class MazeGeneratorManager {
+
+    public:
+        inline static constexpr size_t s_MinSteps           = 1;
+        inline static constexpr size_t s_MaxSteps           = 1 << 14;
+        inline static constexpr float  s_MinUpdateTimeframe = 1.0F / 30.0F;
+
+    private:
+        MazeGenerator m_Generator;
+        size_t        m_StepsPerUpdate;
+        bool          m_IsPaused;
+        float         m_Theta = 0.0;
+
+    public:
+        MazeGeneratorManager();
+
+    public:
+        void update(app::Application* app, Maze2D& maze, float delta);
+
+    };
+
+} // maze
+
+#endif
